f55ed94191b4814a40ba54829b067f7871ef7671
Aries <ajtagle12@gmail.com>
2023-10-17 13:08:58 +0800

    added seeder for database

---
+++ database/seeders/UsersTableSeeders.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //default value(runs when you make the migration command)
+        DB::table('users')->insert([
+            // admin
+            [
+                "name" => 'Admin',
+                "username" => 'admin',
+                "email" => 'admin@gmail.com',
+                "password" => Hash::make('123'),
+                "role" => 'admin',
+            ],
+            // user
+            [
+                "name" => 'User',
+                "username" => 'user',
+                "email" => 'user@gmail.com',
+                "password" => Hash::make('123'),
+                "role" => 'user',
+            ],
+        ]);
+    }
+}
