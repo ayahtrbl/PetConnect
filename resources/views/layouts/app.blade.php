b862fc5ccd4f04588d3827b34c7da7f5b4ba3eba
Aries <ajtagle12@gmail.com>
2023-10-17 19:07:43 +0800

    added a new file for admin switch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,18 +31,8 @@
             <main>
                 {{ $slot }}
             </main>
+            @include('layouts.admin-switch')
         </div>
-        @auth
-            @if (auth()->user()->role === 'admin')
-                <div class="flex gap-2 fixed bottom-[60px] right-[60px]">
-                    <x-admin-user-switch href="{{ route('admin.dashboard') }}" :active="Str::contains(request()->path(), 'admin')">
-                        Admin
-                    </x-admin-user-switch>
-                    <x-admin-user-switch href="{{ route('dashboard') }}" :active="!Str::contains(request()->path(), 'admin')">
-                        User
-                    </x-admin-user-switch>
-                </div>
-            @endif
-        @endauth
+        
     </body>
 </html>
