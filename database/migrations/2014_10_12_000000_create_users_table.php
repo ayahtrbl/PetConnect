7cfdb258fb8ceff2de6f9e26558ee470f2654692
Aries <ajtagle12@gmail.com>
2023-10-17 13:33:26 +0800

    added factory

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('photo');
-            $table->string('address');
+            $table->string('photo')->nullable();
+            $table->string('address')->nullable();
             $table->enum('role', ['admin','user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // runs the seeders
+        $this->call(UsersTableSeeders::class);
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
