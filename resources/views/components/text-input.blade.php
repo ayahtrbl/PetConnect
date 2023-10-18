6b8d2e63c9721a320247728cbef0893148b8dd96
Aries <ajtagle12@gmail.com>
2023-10-19 00:37:39 +0800

    fixed border focus

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'placeholder:text-[#ffb845] bg-white/0 border-indigo-500/0 border-b-[#ffb845]  rounded-md']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'placeholder:text-[#ffb845] bg-white/0 border-indigo-500/0 border-b-[#ffb845] focus:border-indigo-500/0 focus:border-b-[#ffb845] focus:outline-none focus:ring-0 rounded-md']) !!}>
