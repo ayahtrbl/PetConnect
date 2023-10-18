e01b887caf8cff1d38f01ce9345106fe02c6e678
Aries <ajtagle12@gmail.com>
2023-10-19 00:45:51 +0800

    Registration page is done in editing

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,43 +7,47 @@
     <div class = "flex flex-col flex-1 justify-center items-center">
         <form method="POST" action="{{ route('register') }}">
             @csrf
+
+            <div class="mb-10">
+                <h2 class = "text-4xl font-bold">Register</h2>
+                <br>
+                <p class =>Let's Get Started!
+                    <br>Create an account to login.</p>
+            </div>
+
             <!-- Name -->
             <div>
-                <x-input-label for="name" :value="__('Name')" />
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-text-input  id="name" placeholder="Name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
             <!-- Email Address -->
             <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
+                <x-text-input id="email" placeholder="Email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
             <!-- Birthday-->
             <div class="mt-4">
-                <x-input-label for="birthday" :value="__('Birthday')" />
-                <x-date-input id="birthday" class="block mt-1 w-full" type="date" name="birthday" :value="old('birthday')" required />
+                <x-text-input id="birthday" class="block mt-1 w-full" type="date" name="birthday" :value="old('birthday')" required />
                 <x-input-error :messages="$errors->get('birthday')" class="mt-2" />
             </div>
             <!-- Password -->
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
                 <x-text-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="new-password" />
+                                placeholder="Password"
+                                required/>
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
                 <x-text-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
+                                name="password_confirmation" required placeholder="Confirmed Password" />
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                <a class="underline text-sm hover:text-[#ffb845] rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
                 <x-primary-button class="ml-4">
