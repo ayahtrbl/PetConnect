35eba50a63d0d19b0ad602771d33a6313ba3a2f2
Aries <ajtagle12@gmail.com>
2023-10-19 01:56:27 +0800

    fixed forgot-password design

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,33 @@
 <x-guest-layout>
     
-    
-    <form method="POST" action="{{ route('password.email') }}">
+    <div class = "hidden lg:block flex-1 p-6 justify-center items-center">
+        <img src="{{asset('images/photo-deco-3.png')}}" alt="happy dog">
+    </div>
+
+    <div class = "flex flex-col flex-1 justify-center items-center">
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-        @csrf
-        <!-- Email Address -->
-        <div class="mb-4 text-sm">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
-        <div>
-            <x-text-input id="email" placehold="Email" title="Email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-        <div class="flex items-center justify-end mt-4 gap-6">
-            <a href="{{route('login')}}" class = "text-white hover:text-[#ffb845] underline text-sm">
-                Go back to login
-            </a>
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <!-- Email Address -->
+            <h1 class = "text-4xl mb-5">
+                Reset Password
+            </h1>
+            <div class="mb-4 text-sm">
+                Forgot your password? No problem. Just let us know your <span class="font-bold">email address</span> and we will email you a password reset link that will allow you to choose a new one.
+            </div>
+            <div>
+                <x-text-input id="email" placeholder="Email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+            <div class="flex items-center justify-end mt-4 gap-6">
+                <a href="{{route('login')}}" class = "text-white hover:text-[#ffb845] underline text-sm">
+                    Go back to login
+                </a>
+                <x-primary-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </div>
 </x-guest-layout>
