db139b5bf91f8f06ee5276be110a4d9b00bbc319
Aries <ajtagle12@gmail.com>
2023-10-19 22:55:28 +0800

    fixed landing page and logo from other pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,19 +15,26 @@
         <link rel="stylesheet" href="{{asset('css/landing_page_darkmode.css')}}">
     </head>
     <body>
+      <form id="logout" method="post" action="{{ route('logout') }}" class="search-form">
+        @csrf
+      </form>
         <header>
           <nav class="navbar">
             <a href="/" class="logo">
-              <img src="{{asset('images/logo.png')}}" alt="Pet Connect" class = "w-16 h-16">
+              <img src="{{asset('images/logo.png')}}" alt="Pet Connect" class = "w-auto h-12">
             </a>
             <ul class="menu-links">
               @auth
-              <li><a href="#">Home</a></li>
+              <li><a href="{{route('dashboard')}}" >Home</a></li>
               <li><a href="#">Adopt</a></li>
               <li><a href="#">Donate</a></li>
-              @endauth
+              <li >
+                <button form="logout" class = "hover:bg-green-500 hover:border-white/0 text-white px-3 py-2 rounded-md border-2 ease-in-out duration-100 border-white">Log Out</button>
+              </li>
+              @else
               <li><a href="{{route('login')}}">Sign In</a></li>
               <li class="join-btn"><a href="{{route('register')}}">Register</a></li>
+              @endauth
               <span id="close-menu-btn" class="material-symbols-outlined">close</span>
             </ul>
             <span id="hamburger-btn" class="material-symbols-outlined">menu</span>
