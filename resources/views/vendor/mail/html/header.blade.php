080ed44c9dd243d2be3f8223861c05f9c8c23239
Aries <ajtagle12@gmail.com>
2023-10-19 23:28:45 +0800

    fixed navigation dashboard active

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Pet Connect')
-<img src="https://raw.githubusercontent.com/AriesJ12/PetConnect/main/public/images/logo.png?token=GHSAT0AAAAAACHJ54JIESNBKBABRXMJOQLGZJQWRTA" class="logo" alt="Pet Connect Logo">
+<img src="https://raw.githubusercontent.com/AriesJ12/PetConnect/main/public/images/logo.png?token=GHSAT0AAAAAACHJ54JIV634AQSVWV5DWDJOZJRLDEA" class="logo" alt="Pet Connect Logo">
 @else
 {{ $slot }}
 @endif
